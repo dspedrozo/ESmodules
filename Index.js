adf6ba5e4d02222e65d64ed7665bd58cc5f92ebf
Dani <danielapedrozo@gmail.com>
2022-07-27 22:40:33 -0300

    Ejemplo de uso ED module para importar

---
+++ Index.js
@@ -0,0 +1,10 @@
+import * as operaciones from "./Operaciones.js";
+
+
+console.log("Suma: " + operaciones.addition(8,2));
+
+console.log("Sustracción: " + operaciones.subtraction(8,2));
+
+console.log("Multiplicación: "+ operaciones.multiplication(8,2));
+
+console.log("División: "+ operaciones.division(8,2));
---
+++ Operaciones.js
@@ -0,0 +1,11 @@
+var addition = (x,y) => x+y;
+var subtraction = (x,y) => x-y;
+var division = (x,y) => x/y;
+var multiplication = (x,y) => x*y;
+
+export {
+    addition,
+    subtraction,
+    division,
+    multiplication
+}
